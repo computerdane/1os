e30c23809858d1008a2315565392be85e03c5076
Dane Rieber <danerieber@gmail.com>
2024-10-22 09:48:24 -0500

    update factorio package to 2.0.8

--- features/server/factorio.nix
+++ features/server/factorio.nix
@@ -1,8 +1,25 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  factorio-headless-latest = pkgs.factorio-headless-experimental.override {
+    versionsJson = pkgs.writeText "versions.json" (
+      builtins.toJSON {
+        x86_64-linux.headless.experimental = {
+          name = "factorio_headless_x64-2.0.8.tar.xz";
+          needsAuth = false;
+          sha256 = "sha256-2VlMTVUqPk+WWxiKR3TajIsBD8I92w78Y7HZSBjd4co=";
+          tarDirectory = "x64";
+          url = "https://www.factorio.com/get-download/2.0.8/headless/linux64";
+          version = "2.0.8";
+        };
+      }
+    );
+  };
+in
 {
   services.factorio = {
     enable = true;
+    package = factorio-headless-latest;
     openFirewall = true;
     game-password = "sex";
     lan = true;
