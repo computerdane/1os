d9f442c298e12c7184d39b19e9e1763694deeb8c
Dane Rieber <danerieber@gmail.com>
2024-10-21 23:31:02 -0500

    ipv6 prefix delegation

--- features/server.nix
+++ features/server.nix
@@ -33,6 +33,18 @@
       "10.0.105.10" = [ "one.lan" ];
     };
     hostId = "c04107a1"; # required by ZFS to ensure that a pool isn't accidentally imported on a wrong machine
+    dhcpcd = {
+      persistent = true;
+      allowInterfaces = [ "wan" ];
+      extraConfig = ''
+        noipv6rs        # disable routing solicitation
+        interface wan
+          ipv6rs        # enable routing solicitation for wan
+          ia_na 1       # request an IPv6 address
+          ia_pd 2 lan/0 # request a PD and assign it to lan
+          ia_pd 3 pc/0  # request a PD and assign it to pc
+      '';
+    };
   };
 
   services.dnsmasq = {
@@ -41,6 +53,37 @@
       server = [
         "1.1.1.1"
         "1.0.0.1"
+      ];
+
+      # If you want dnsmasq to listen for DHCP and DNS requests only on
+      # specified interfaces (and the loopback) give the name of the
+      # interface (eg eth0) here.
+      # Repeat the line for more than one interface.
+      #interface=
+      # Or you can specify which interface _not_ to listen on
+      except-interface = "wan";
+
+      # Set the DHCP server to authoritative mode. In this mode it will barge in
+      # and take over the lease for any client which broadcasts on the network,
+      # whether it has a record of the lease or not. This avoids long timeouts
+      # when a machine wakes up on a new network. DO NOT enable this if there's
+      # the slightest chance that you might end up accidentally configuring a DHCP
+      # server for your campus/company accidentally. The ISC server uses
+      # the same option, and this URL provides more information:
+      # http://www.isc.org/files/auth.html
+      dhcp-authoritative = true;
+
+      dhcp-range = [
+        # Do stateless DHCP, SLAAC, and generate DNS names for SLAAC addresses
+        # from DHCPv4 leases.
+        "::,ra-stateless,ra-names"
+
+        # Uncomment this to enable the integrated DHCP server, you need
+        # to supply the range of addresses available for lease and optionally
+        # a lease time. If you have more than one network, you will need to
+        # repeat this for each network on which you want to supply DHCP
+        # service.
+        "10.0.105.50,10.0.105.150,12h"
       ];
 
       # Set the domain for dnsmasq. this is optional, but if it is set, it
@@ -52,31 +95,10 @@
       # 3) Provides the domain part for "expand-hosts"
       domain = "one.lan";
 
-      dhcp-range = [
-        # Do stateless DHCP, SLAAC, and generate DNS names for SLAAC addresses
-        # from DHCPv4 leases.
-        "::, ra-stateless, ra-names"
-
-        # Uncomment this to enable the integrated DHCP server, you need
-        # to supply the range of addresses available for lease and optionally
-        # a lease time. If you have more than one network, you will need to
-        # repeat this for each network on which you want to supply DHCP
-        # service.
-        "10.0.105.50,10.0.105.150,12h"
-      ];
-
       # Always set the name of the host with hardware address
       # 11:22:33:44:55:66 to be "fred"
       #dhcp-host=11:22:33:44:55:66,fred
       dhcp-host = [ "9c:6b:00:2f:0e:be,pc" ];
-
-      # If you want dnsmasq to listen for DHCP and DNS requests only on
-      # specified interfaces (and the loopback) give the name of the
-      # interface (eg eth0) here.
-      # Repeat the line for more than one interface.
-      #interface=
-      # Or you can specify which interface _not_ to listen on
-      except-interface = "wan";
 
       # Never forward plain names (without a dot or domain part)
       domain-needed = true;
