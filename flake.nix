a7270e7dfe69ea3c38ce9a64625f47076362957f
dane <danerieber@gmail.com>
2024-10-28 21:50:04 -0500

    set system variable per-machine

--- flake.nix
+++ flake.nix
@@ -10,17 +10,17 @@
       nixpkgs,
       nixpkgs-unstable,
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      pkgs-unstable = import nixpkgs-unstable { inherit system; };
-      pkgs-1os = pkgs.callPackage ./packages/all-packages.nix { };
-    in
     {
       nixosConfigurations =
         builtins.mapAttrs
           (
-            name: value:
+            name: cfg:
+            with cfg;
+            let
+              pkgs = import nixpkgs { inherit system; };
+              pkgs-unstable = import nixpkgs-unstable { inherit system; };
+              pkgs-1os = pkgs.callPackage ./packages/all-packages.nix { };
+            in
             nixpkgs.lib.nixosSystem {
               inherit system;
               modules = [
@@ -31,30 +31,36 @@
                     networking.hostName = name;
                   }
                 )
-              ] ++ value;
+              ] ++ modules;
               specialArgs = {
                 inherit pkgs-unstable pkgs-1os;
               };
             }
           )
           {
-            fishtank = [
-              ./hardware/fishtank.nix
+            fishtank = {
+              system = "x86_64-linux";
+              modules = [
+                ./hardware/fishtank.nix
 
-              ./features/desktop.nix
-              ./features/gaming.nix
-            ];
-            bludgeonder = [
-              ./hardware/bludgeonder.nix
+                ./features/desktop.nix
+                ./features/gaming.nix
+              ];
+            };
+            bludgeonder = {
+              system = "x86_64-linux";
+              modules = [
+                ./hardware/bludgeonder.nix
 
-              ./features/auto-update/pull.nix
-              ./features/auto-update/push.nix
+                ./features/auto-update/pull.nix
+                ./features/auto-update/push.nix
 
-              ./features/gateway.nix
+                ./features/gateway.nix
 
-              ./features/factorio-server.nix
-              ./features/quilt-server.nix
-            ];
+                ./features/factorio-server.nix
+                ./features/quilt-server.nix
+              ];
+            };
           };
     };
 }
