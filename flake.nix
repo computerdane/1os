19ff0a37dde92e28c2a980d590167e44559bf2a0
Dane Rieber <danerieber@gmail.com>
2024-10-28 21:55:35 -0500

    move host configs to let-in statement

--- flake.nix
+++ flake.nix
@@ -10,57 +10,60 @@
       nixpkgs,
       nixpkgs-unstable,
     }:
+    let
+      hosts = {
+
+        bludgeonder = {
+          system = "x86_64-linux";
+          modules = [
+            ./hardware/bludgeonder.nix
+
+            ./features/auto-update/pull.nix
+            ./features/auto-update/push.nix
+
+            ./features/gateway.nix
+
+            ./features/factorio-server.nix
+            ./features/quilt-server.nix
+          ];
+        };
+
+        fishtank = {
+          system = "x86_64-linux";
+          modules = [
+            ./hardware/fishtank.nix
+
+            ./features/desktop.nix
+            ./features/gaming.nix
+          ];
+        };
+
+      };
+    in
     {
-      nixosConfigurations =
-        builtins.mapAttrs
-          (
-            name: cfg:
-            with cfg;
-            let
-              pkgs = import nixpkgs { inherit system; };
-              pkgs-unstable = import nixpkgs-unstable { inherit system; };
-              pkgs-1os = pkgs.callPackage ./packages/all-packages.nix { };
-            in
-            nixpkgs.lib.nixosSystem {
-              inherit system;
-              modules = [
-                ./configuration.nix
-                (
-                  { ... }:
-                  {
-                    networking.hostName = name;
-                  }
-                )
-              ] ++ modules;
-              specialArgs = {
-                inherit pkgs-unstable pkgs-1os;
-              };
-            }
-          )
-          {
-            fishtank = {
-              system = "x86_64-linux";
-              modules = [
-                ./hardware/fishtank.nix
-
-                ./features/desktop.nix
-                ./features/gaming.nix
-              ];
-            };
-            bludgeonder = {
-              system = "x86_64-linux";
-              modules = [
-                ./hardware/bludgeonder.nix
-
-                ./features/auto-update/pull.nix
-                ./features/auto-update/push.nix
-
-                ./features/gateway.nix
-
-                ./features/factorio-server.nix
-                ./features/quilt-server.nix
-              ];
-            };
+      nixosConfigurations = builtins.mapAttrs (
+        name: cfg:
+        with cfg;
+        let
+          pkgs = import nixpkgs { inherit system; };
+          pkgs-unstable = import nixpkgs-unstable { inherit system; };
+          pkgs-1os = pkgs.callPackage ./packages/all-packages.nix { };
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            (
+              { ... }:
+              {
+                networking.hostName = name;
+              }
+            )
+          ] ++ modules;
+          specialArgs = {
+            inherit pkgs-unstable pkgs-1os;
           };
+        }
+      ) hosts;
     };
 }
