c0debf0146d38fabad5d144e119708cc05c8d978
Dane Rieber <danerieber@gmail.com>
2024-11-03 13:57:30 -0600

    move some hard coded values into options

--- modules/gateway.nix
+++ modules/gateway.nix
@@ -4,112 +4,154 @@ let
   cfg = config.oneos.gateway;
 in
 {
-  options.oneos.gateway.enable = lib.mkEnableOption "gateway";
-
-  config = lib.mkIf cfg.enable {
-    sops.secrets.gateway-wireguard-key = {
-      owner = config.users.users.systemd-network.name;
-      group = config.users.users.systemd-network.group;
+  options.oneos.gateway =
+    with lib;
+    with types;
+    {
+      enable = mkEnableOption "gateway";
+      lan = {
+        ipv4 = {
+          address = mkOption {
+            type = str;
+            default = "10.0.105.1";
+          };
+          prefixLength = mkOption {
+            type = int;
+            default = 24;
+          };
+        };
+        ipv6 = {
+          address = mkOption {
+            type = str;
+            default = "fd00:da2e::1";
+          };
+        };
+      };
+      wireguard = {
+        ipv4 = {
+          subnet = mkOption {
+            type = str;
+            default = "10.0.36.0/24";
+          };
+        };
+        port = mkOption {
+          type = port;
+          default = 50105;
+        };
+      };
+      dns = mkOption {
+        type = listOf str;
+        default = [
+          "1.1.1.1"
+          "1.0.0.1"
+        ];
+      };
     };
 
-    systemd.network = {
-      networks."25-wg" = {
-        name = "wg";
-        routes = [
-          {
-            routeConfig = {
-              PreferredSource = "10.0.105.1";
-              Destination = "10.0.36.0/24";
+  config =
+    let
+      lanIpv4Cidr = with cfg.lan.ipv4; "${address}/${toString prefixLength}";
+    in
+    lib.mkIf cfg.enable {
+      sops.secrets.gateway-wireguard-key = {
+        owner = config.users.users.systemd-network.name;
+        group = config.users.users.systemd-network.group;
+      };
+
+      systemd.network = {
+        networks = {
+          "10-wan" = {
+            name = "wan";
+            DHCP = "yes";
+            networkConfig.IPv6AcceptRA = true;
+            dhcpV6Config = {
+              WithoutRA = "solicit";
+              UseDNS = false;
             };
-          }
-        ];
-      };
-      netdevs."25-wg" = {
-        netdevConfig = {
-          Kind = "wireguard";
-          Name = "wg";
-        };
-        wireguardConfig = {
-          ListenPort = 50105;
-          PrivateKeyFile = config.sops.secrets.gateway-wireguard-key.path;
-        };
-        wireguardPeers = [
-          {
-            wireguardPeerConfig = {
-              AllowedIPs = [ "10.0.36.0/24" ];
-              PublicKey = "W9WHvF9Z8DNpMHVgYfcvY/ep93iC/R4PJKcQr0ty3RA="; # schlaptop
+            ipv6AcceptRAConfig.UseDNS = false;
+            dhcpV4Config.UseDNS = false;
+          };
+          "20-lan" = {
+            name = "lan";
+            DHCP = "no";
+            networkConfig = {
+              Address = [
+                lanIpv4Cidr
+                cfg.lan.ipv6.address
+              ];
+              DNS = cfg.dns;
+              DHCPServer = true;
+              DHCPPrefixDelegation = true;
+              IPv6SendRA = true;
+              IPv6AcceptRA = false;
             };
-          }
-        ];
-      };
-    };
-
-    systemd.network.networks = {
-      "10-wan" = {
-        name = "wan";
-        DHCP = "yes";
-        networkConfig.IPv6AcceptRA = true;
-        dhcpV6Config = {
-          WithoutRA = "solicit";
-          UseDNS = false;
+            dhcpServerConfig.PoolOffset = 100;
+            dhcpPrefixDelegationConfig = {
+              UplinkInterface = "wan";
+              Announce = "yes";
+            };
+          };
+          "25-wg" = {
+            name = "wg";
+            routes = [
+              {
+                routeConfig = {
+                  PreferredSource = lanIpv4Cidr;
+                  Destination = cfg.wireguard.ipv4.subnet;
+                };
+              }
+            ];
+          };
         };
-        ipv6AcceptRAConfig.UseDNS = false;
-        dhcpV4Config.UseDNS = false;
-      };
-      "20-lan" = {
-        name = "lan";
-        DHCP = "no";
-        networkConfig = {
-          Address = "10.0.105.1/24";
-          DNS = [
-            "1.1.1.1"
-            "1.0.0.1"
-          ];
-          DHCPServer = true;
-          DHCPPrefixDelegation = true;
-          IPv6SendRA = true;
-          IPv6AcceptRA = false;
-        };
-        dhcpServerConfig.PoolOffset = 100;
-        dhcpPrefixDelegationConfig = {
-          UplinkInterface = "wan";
-          Announce = "yes";
-        };
-      };
-    };
-
-    networking = {
-      firewall = {
-        interfaces = {
-          lan.allowedUDPPorts = [
-            53 # dns
-            67 # dhcp
-          ];
-          wan.allowedUDPPorts = [
-            50105 # wireguard
+        netdevs."25-wg" = {
+          netdevConfig = {
+            Kind = "wireguard";
+            Name = "wg";
+          };
+          wireguardConfig = {
+            ListenPort = cfg.wireguard.port;
+            PrivateKeyFile = config.sops.secrets.gateway-wireguard-key.path;
+          };
+          wireguardPeers = [
+            {
+              wireguardPeerConfig = {
+                AllowedIPs = [ cfg.wireguard.ipv4.subnet ];
+                PublicKey = "W9WHvF9Z8DNpMHVgYfcvY/ep93iC/R4PJKcQr0ty3RA="; # schlaptop
+              };
+            }
           ];
         };
-        trustedInterfaces = [ "wg" ];
       };
-      nat = {
-        enable = true;
-        internalInterfaces = [
-          "lan"
-          "wg"
-        ];
-        externalInterface = "wan";
+
+      networking = {
+        firewall = {
+          interfaces = {
+            lan.allowedUDPPorts = [
+              53 # dns
+              67 # dhcp
+            ];
+            wan.allowedUDPPorts = [ cfg.wireguard.port ];
+          };
+          trustedInterfaces = [ "wg" ];
+        };
+        nat = {
+          enable = true;
+          internalInterfaces = [
+            "lan"
+            "wg"
+          ];
+          externalInterface = "wan";
+        };
+        hostId = "c04107a1"; # required by ZFS to ensure that a pool isn't accidentally imported on a wrong machine
       };
-      hosts."10.0.105.1" = [ "one.lan" ];
-      hostId = "c04107a1"; # required by ZFS to ensure that a pool isn't accidentally imported on a wrong machine
+
+      services.openssh.ports = [ 105 ];
+
+      boot.kernel.sysctl = {
+        "net.ipv6.conf.all.forwarding" = 1;
+        "net.ipv4.conf.all.forwarding" = 1;
+      };
+
+      system.stateVersion = lib.mkForce "23.05";
     };
-
-    services.openssh.ports = [ 105 ];
-
-    boot.kernel.sysctl = {
-      "net.ipv6.conf.all.forwarding" = 1;
-      "net.ipv4.conf.all.forwarding" = 1;
-    };
-
-    system.stateVersion = lib.mkForce "23.05";
-  };
 }
