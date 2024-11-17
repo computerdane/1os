437e4d7d5bf7ba43c917c334eea20f094afd2f6c
Dane Rieber <danerieber@gmail.com>
2024-11-16 20:03:09 -0600

    update bop

--- packages/bop.nix
+++ packages/bop.nix
@@ -1,4 +1,5 @@
 {
+  bat,
   mpv,
   netcat,
   writeShellApplication,
@@ -7,10 +8,15 @@
 writeShellApplication {
   name = "bop";
   runtimeInputs = [
+    bat
     mpv
     netcat
   ];
   text = ''
-    echo "$*" | nc nf6.sh 8085 -w1 | xargs -d '\n' -n 1000 mpv --volume=50
+    if [ "$1" == "-l" ]; then
+      echo "-s" | nc nf6.sh 8085 -w1 | bat
+      exit 0
+    fi
+      echo "$*" | tr -d "-" | nc nf6.sh 8085 -w1 | xargs -d '\n' -n 1000 mpv --volume=50
   '';
 }
