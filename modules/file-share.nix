715bc7d9aecd365aa49da0e5895cb3d4ff78f135
Dane Rieber <danerieber@gmail.com>
2024-12-10 15:30:57 -0600

    spin up file server

--- modules/file-share.nix
+++ modules/file-share.nix
@@ -4,13 +4,53 @@ let
   cfg = config.oneos.file-share;
 in
 {
-  options.oneos.file-share.enable = lib.mkEnableOption "file-share";
-
-  config = lib.mkIf cfg.enable {
-    services.diod = {
-      enable = true;
-      exports = [ "/nas/share" ];
-      userdb = true;
+  options.oneos.file-share =
+    with lib;
+    with types;
+    {
+      enable = lib.mkEnableOption "file-share";
+      domain = mkOption {
+        type = str;
+        default = config.oneos.domains.default;
+      };
+      subdomain = mkOption {
+        type = str;
+        default = "f";
+      };
+      path = mkOption {
+        type = str;
+        default = "/nas/share";
+      };
     };
-  };
+
+  config =
+    let
+      domain = "${cfg.subdomain}.${cfg.domain}";
+    in
+    lib.mkIf cfg.enable {
+      # services.diod = {
+      #   enable = true;
+      #   exports = [ "/nas/share" ];
+      #   userdb = true;
+      # };
+
+      oneos = {
+        acme.enable = true;
+        dynamic-dns = {
+          enable = true;
+          ipv4 = true;
+          subdomains = [ cfg.subdomain ];
+          domains = [ cfg.domain ];
+        };
+        nginx.enable = true;
+      };
+
+      services.nginx = {
+        virtualHosts.${domain} = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/".root = cfg.path;
+        };
+      };
+    };
 }
