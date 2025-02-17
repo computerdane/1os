dcff85127564f760b7ea7154562658c526427869
Dane Rieber <danerieber@gmail.com>
2025-02-17 17:36:07 -0600

    add specialization for amdvlk drivers

--- hosts/fishtank.nix
+++ hosts/fishtank.nix
@@ -6,11 +6,12 @@
     desktop.enable = true;
     dynamic-dns.enable = true;
     gaming.enable = true;
-    gpu-amd = {
-      enable = true;
-      # useWeirdLibs = true;
-    };
+    gpu-amd.enable = true;
     # virtualisation.enable = true;
+  };
+
+  specialisation.amdvlk.configuration = {
+    oneos.gpu-amd.useWeirdLibs = true;
   };
 
   environment.systemPackages = with pkgs; [
