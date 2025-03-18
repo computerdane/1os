6f9a6f9b3d3a7677aee753b4501cfd9707a22286
Dane Rieber <danerieber@gmail.com>
2025-03-17 21:52:15 -0500

    fix 9p mount

--- modules/mount-9p.nix
+++ modules/mount-9p.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.oneos.mount-9p;
@@ -31,16 +36,25 @@ in
       userdb = true;
     };
 
-    fileSystems = lib.mkIf (!cfg.isHost) {
-      ${cfg.path} = {
-        device = cfg.host;
-        fsType = "9p";
-        options = [
-          "aname=${cfg.path}"
-          "version=9p2000.L"
-          "uname=root"
-          "access=any"
-        ];
+    systemd.services."mount-nas" = lib.mkIf (!cfg.isHost) {
+      wantedBy = [ "multi-user.target" ];
+      requires = [ "network-online.target" ];
+      path = with pkgs; [
+        mount
+        umount
+      ];
+      script = ''
+        mkdir -p "${cfg.path}"
+        mount -t 9p -n "${cfg.host}" "${cfg.path}" "-oaname=${cfg.path},version=9p2000.L,uname=root,access=any,trans=tcp"
+      '';
+      preStop = ''
+        umount -l "${cfg.path}"
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+        Restart = "on-failure";
+        RestartSec = "10s";
       };
     };
 
