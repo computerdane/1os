8e1a8d76d269e1923e790d71300b468e9ca236b0
Dane Rieber <danerieber@gmail.com>
2025-03-21 14:10:56 -0500

    both flake and non-flake solution

--- flake.nix
+++ flake.nix
@@ -1,4 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-  outputs = { nixpkgs, ... }: import ./default.nix { inherit nixpkgs; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    onix.url = "github:computerdane/onix/no-more-flakes";
+    sops-nix.url = "github:Mic92/sops-nix";
+  };
+  outputs =
+    {
+      nixpkgs,
+      onix,
+      sops-nix,
+      ...
+    }:
+    onix.init {
+      inherit nixpkgs;
+      src = ./.;
+      extraModules = [ sops-nix.nixosModules.sops ];
+      nixpkgsConfig.allowUnfree = true;
+    };
 }
