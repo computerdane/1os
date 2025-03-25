c688ff2c93bb615f9dee43ca55325892b2eaef0d
Dane Rieber <danerieber@gmail.com>
2025-03-25 13:09:53 -0500

    setup limbo for john/aria

--- configs/limbo/configuration.nix
+++ configs/limbo/configuration.nix
@@ -4,15 +4,18 @@
   imports = [ ./hardware-configuration.nix ];
 
   oneos = {
-    acme.enable = true;
+    # acme.enable = true;
     # auto-update.pull = true;
+    desktop.enable = true;
     dynamic-dns.enable = true;
+    extra-users.enable = true;
+    gaming.enable = true;
     gpu-nvidia.enable = true;
-    jellyfin = {
-      enable = true;
-      subdomain = "watch-test";
-    };
-    mount-9p.enable = true;
-    nginx.enable = true;
+    # jellyfin = {
+    #   enable = true;
+    #   subdomain = "watch-test";
+    # };
+    # mount-9p.enable = true;
+    # nginx.enable = true;
   };
 }
--- hosts/limbo.nix
+++ hosts/limbo.nix
@@ -1,3 +1,10 @@
 {
   system = "x86_64-linux";
+  users.dane.hm-configs = [
+    "full"
+    "hll"
+    "kde"
+  ];
+  users.john.hm-configs = [ "full" ];
+  users.aria.hm-configs = [ "full" ];
 }
