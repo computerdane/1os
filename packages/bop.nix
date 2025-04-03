0f0df83598d55760e2a55f174c616f17000900e8
dane <danerieber@gmail.com>
2025-04-02 19:31:59 -0500

    bop: 0.1.1 -> 0.1.2

--- packages/bop.nix
+++ packages/bop.nix
@@ -5,15 +5,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "bop";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "computerdane";
     repo = "bop";
     rev = version;
-    hash = "sha256-aOkfkmK+l3WoamxCd0DO5jCu0AwTrUXGlbYvLd3Ykaw=";
+    hash = "sha256-jaUmZL9cPUlwDqNE8pXPNBYIKRhRYv/J43hPvdt9n8s=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-oyJdkQ3RDekPWVmjyJhXX+fBLnnP1mleZ/b8gHajLWA=";
+  cargoHash = "sha256-GM73f5NYZsjdNTJZZiHSv6Kmjr3MkB6GF/BidIHcwKE=";
 }
