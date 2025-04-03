2525f5d7513781b9254412cc4bb8e88ee1c27758
dane <danerieber@gmail.com>
2025-04-02 23:48:16 -0500

    gaming hm module

---
+++ hm-modules/gaming.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (pkgs) stdenv;
+  cfg = config.oneos.gaming;
+in
+{
+  options.oneos.gaming.enable = lib.mkEnableOption "gaming";
+
+  config =
+    with lib;
+    mkIf cfg.enable (mkMerge [
+
+      {
+        home.packages = with pkgs; [
+          hll-arty-calc
+          hll-arty-tui
+          mc-quick
+        ];
+      }
+
+      (mkIf stdenv.isLinux {
+        home.packages = [ pkgs.prismlauncher ];
+        programs.obs-studio.enable = true;
+      })
+
+    ]);
+}
