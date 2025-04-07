40cf89bc443a06d50bd17cf41255da8714876379
dane <danerieber@gmail.com>
2025-04-07 14:20:04 -0500

    disable using mold

--- hm-modules/development.nix
+++ hm-modules/development.nix
@@ -30,11 +30,11 @@ in
       rust.enable = true;
     };
 
-    home.file.".cargo/config.toml".text = ''
-      [target.x86_64-unknown-linux-gnu]
-      linker = "${pkgs.clang}/bin/clang"
-      rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
-    '';
+    # home.file.".cargo/config.toml".text = ''
+    #   [target.x86_64-unknown-linux-gnu]
+    #   linker = "${pkgs.clang}/bin/clang"
+    #   rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
+    # '';
 
   };
 }
