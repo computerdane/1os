b8cad3c6b01040b70f386370b0009a45804abae4
Dane Rieber <danerieber@gmail.com>
2025-04-23 18:38:19 -0500

    vintagestory-server service

---
+++ modules/vintagestory-server.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.oneos.vintagestory-server;
+in
+{
+  options.oneos.vintagestory-server =
+    with lib;
+    with types;
+    {
+      enable = mkEnableOption "vintagestory-server";
+      port = mkOption {
+        type = port;
+        default = 42420;
+      };
+      openFirewall = mkEnableOption "open firewall";
+    };
+
+  config = lib.mkIf cfg.enable {
+
+    networking.firewall = lib.mkIf cfg.openFirewall {
+      allowedTCPPorts = [ cfg.port ];
+      allowedUDPPorts = [ cfg.port ];
+    };
+
+    systemd.services.vintagestory-server = {
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig.DynamicUser = true;
+      path = [ pkgs.vintagestory ];
+      script = ''
+        vintagestory-server --dataPath "$STATE_DIRECTORY" --port ${toString cfg.port}
+      '';
+    };
+
+  };
+}
