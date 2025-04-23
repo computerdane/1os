24c6120f5a7aae5a20960e370ca4ed17bf58067c
dane <danerieber@gmail.com>
2025-04-23 17:34:00 -0500

    remove leftover scripts

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-nixos-rebuild switch -f default.nix -A $1 --target-host $2 --use-remote-sudo
--- hmdeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-home-manager build -f default.nix -A $1 && \
-  nix-copy-closure --to $2 result && \
-  ssh -t $2 "$(readlink result)/activate"
