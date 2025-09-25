b36e32be2b10f0aa843aa460471f11309b0a1b1c
Dane Rieber <danerieber@gmail.com>
2025-09-25 14:36:38 -0500

    pipewire loopback for headset

--- configs/fishtank/configuration.nix
+++ configs/fishtank/configuration.nix
@@ -23,4 +23,28 @@
     mumble
     stress-ng
   ];
+
+  services.pipewire.extraConfig.pipewire = {
+    "10-loopback" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-loopback";
+          args = {
+            "node.description" = "Loopback to Headset";
+            "capture.props" = {
+              "node.name" = "loopback_to_headset";
+              "media.class" = "Audio/Sink";
+              "audio.position" = "[ FL FR ]";
+            };
+            "playback.props" = {
+              "node.name" = "playback.loopback_to_headset";
+              "audio.position" = "[ FL FR ]";
+              "target.object" =
+                "alsa_output.usb-HP__Inc_HyperX_Cloud_III_Wireless_0000000000000000-00.analog-stereo";
+            };
+          };
+        }
+      ];
+    };
+  };
 }
