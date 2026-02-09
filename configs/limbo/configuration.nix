851941842e68192fd92c454381876d91e8dc4481
Dane Rieber <danerieber@gmail.com>
2026-02-09 15:03:56 -0600

    move pinkcraft to limbo

--- configs/limbo/configuration.nix
+++ configs/limbo/configuration.nix
@@ -1,5 +1,14 @@
-{ ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
+let
+  port = 25565;
+  vcPort = 24454;
+in
 {
   imports = [ ./hardware-configuration.nix ];
 
@@ -20,4 +29,36 @@
     dynamic-dns.enable = true;
     extra-users.enable = true;
   };
+
+  services.minecraft-server = {
+    enable = true;
+    eula = true;
+    jvmOpts = "-Xmx8G -Xms8G";
+    package = pkgs.temurin-jre-bin;
+  };
+
+  systemd.services.minecraft-server =
+    let
+      cfg = config.services.minecraft-server;
+    in
+    {
+      serviceConfig = {
+        ExecStart = lib.mkForce "${cfg.package}/bin/java ${cfg.jvmOpts} -jar server.jar nogui";
+      };
+    };
+
+  users.users.minecraft.homeMode = "770";
+  users.users.dane.extraGroups = [ "minecraft" ];
+
+  networking.firewall =
+    let
+      ports = [
+        port
+        vcPort
+      ];
+    in
+    {
+      allowedUDPPorts = ports;
+      allowedTCPPorts = ports;
+    };
 }
